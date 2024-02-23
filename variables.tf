484a1b01ba664a46c9befbcaa346a8a444e05e9a
sreenivasaganesh3 <150334102+sreenivasaganesh3@users.noreply.github.com>
2024-02-23 21:39:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "name"{
+  type = string
+  default = "rsg-si"
+}
+
+variable "location"{
+  type =  string
+  default = "southindia"
+  
+}
+
+variable "virtualnetwork"{
+  type = string
+  default = "vnet2124"
+}
