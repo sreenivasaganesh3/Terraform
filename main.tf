c5dc71e3a159136b458c2f4853e425dc5e015c5d
sreenivasaganesh3 <150334102+sreenivasaganesh3@users.noreply.github.com>
2024-02-23 21:39:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,51 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "myrg21" {
+  name     = var.name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "myvnet" {
+  name                = var.virtualnetwork
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.name
+}
+
+resource "azurerm_subnet" "mysubnet" {
+  count                = 2
+  name                 = "my-subnet-${count.index}"
+  address_prefixes     = ["10.0.${count.index}.0/24"]
+  virtual_network_name = var.virtualnetwork
+  resource_group_name  = var.name
+}
+
+
+resource "azurerm_storage_account" "storage"{
+  count   = 2
+  name                      = "saz21${count.index}"
+  resource_group_name       = var.name
+  location                  = var.location
+  account_tier              =  "Standard"
+  account_replication_type  = "GRS"
+
+
+  tags= {
+     environment  = "developement"
+    }
+}
